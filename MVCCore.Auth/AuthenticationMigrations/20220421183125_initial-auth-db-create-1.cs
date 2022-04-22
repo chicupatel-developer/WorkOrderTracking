3ed566b5c57345ab22cb2e52abc7567853456ea3
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-04-22 12:36:15 -0500

    auth-wip3

---
+++ MVCCore.Auth/AuthenticationMigrations/20220421183125_initial-auth-db-create-1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCore.Auth.AuthenticationMigrations
+{
+    public partial class initialauthdbcreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
