6243e5bbd9bb8664502011ed291059758e399116
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-06-10 12:54:35 -0500

    react-co-co-progress-text-rep-wip2

--- reactapp-wot/src/components/CustomerOrder_Progress_Text_Rep/CustomerOrder_Progress_Text_Rep.jsx
+++ reactapp-wot/src/components/CustomerOrder_Progress_Text_Rep/CustomerOrder_Progress_Text_Rep.jsx
@@ -20,6 +20,7 @@ const CustomerOrder_Progress_Text_Rep = () => {
     CustomerOrderService.getCustomerOrderProgressTextReport(id)
       .then((response) => {
         console.log(response.data);
+        setReportData(response.data);
       })
       .catch((e) => {
         console.log(e);
@@ -43,6 +44,69 @@ const CustomerOrder_Progress_Text_Rep = () => {
     <div className="container">
       <div className="mainHeader">Customer-Order-Progress [Text-Report]</div>
       <hr />
+
+      {reportData.customerOrder ? (
+        <div className="row">
+          <div className="col-md-1 mx-auto"></div>
+          <div className="col-md-5 mx-auto">
+            <div className="card">
+              <div className="card-header">
+                <h4>Customer-Order</h4>
+              </div>
+              <div className="card-body coBody">
+                Customer-Order # {reportData.customerOrder.customerOrderId}
+                <br />
+                Name : {reportData.customerOrder.customerName}
+                <br />
+                Order Qty : {reportData.customerOrder.orderQuantity}
+                <br />
+                Order Date :{" "}
+                {Moment(reportData.customerOrder.orderDate).format(
+                  "DD-MMM-YYYY"
+                )}
+                <br />
+                Order Due Date :{" "}
+                {Moment(reportData.customerOrder.orderDueDate).format(
+                  "DD-MMM-YYYY"
+                )}
+                <br />
+                Days Left #
+              </div>
+            </div>
+          </div>
+          <div className="col-md-5 mx-auto">
+            {reportData.workOrder ? (
+              <div className="card">
+                <div className="card-header">
+                  <h4>Work-Order</h4>
+                </div>
+                <div className="card-body coBody">
+                  Work-Order # {reportData.workOrder.workOrderId}
+                  <br />
+                  Work-Order Status : {reportData.workOrder.workOrderStatus}
+                  <br />
+                  Work-Order Start Date :{" "}
+                  {reportData.workOrder.workOrderStartDate ? (
+                    <span>
+                      {Moment(reportData.workOrder.workOrderStartDate).format(
+                        "DD-MMM-YYYY"
+                      )}
+                    </span>
+                  ) : (
+                    <span>N/A</span>
+                  )}
+                </div>
+              </div>
+            ) : (
+              <span>Work-Order Not Found !</span>
+            )}
+          </div>
+          <div className="col-md-1 mx-auto"></div>
+        </div>
+      ) : (
+        <span>Customer-Order Not Found !</span>
+      )}
+
       <p></p>
     </div>
   );
