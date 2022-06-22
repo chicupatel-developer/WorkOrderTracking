391690e280366983fe2a48f046ab0ba5a8fa9d98
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2022-06-22 12:59:21 -0500

    react-op-wip12

--- reactapp-wot/src/components/OperationLog/OperationLog.jsx
+++ reactapp-wot/src/components/OperationLog/OperationLog.jsx
@@ -9,14 +9,44 @@ import {
   getOperationNumber,
   getOperationStatusForOperator,
 } from "../../services/local.service";
-import CustomerOrderService from "../../services/customerOrder.service";
+import OperationService from "../../services/operation.service";
 import { useNavigate } from "react-router-dom";
 
 import { Table, Button } from "react-bootstrap";
 
 import Moment from "moment";
 
-const OperationLog = ({ operatorLogData, i }) => {
+const OperationLog = () => {
+  let navigate = useNavigate();
+
+  let { id } = useParams();
+
+  const [opLog, setOpLog] = useState([]);
+
+  useEffect(() => {
+    var currRole = AuthService.getCurrentUserRole();
+
+    if (currRole === null || (currRole !== null && currRole !== "Admin"))
+      navigate("/un-auth");
+    else GetOperationLogData(id);
+  }, []);
+
+  const GetOperationLogData = (id) => {
+    OperationService.getOperationLogData(id)
+      .then((response) => {
+        console.log(response.data);
+        setOpLog(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+        if (e.response.status === 401) {
+          console.log("Token Not Found!");
+          AuthService.logout();
+          navigate("/login");
+        }
+      });
+  };
+
   return <div></div>;
 };
 
